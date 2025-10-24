3ce9161b53a4401ee4bcb41273a569b269887be3
Rajnish Subedi <121467364+rajnishsubedi0@users.noreply.github.com>
2025-10-24 16:44:17 +0545

    Update Socket.c

--- Projects/Socket.c
+++ Projects/Socket.c
@@ -7,20 +7,20 @@
 #include <string.h>
 #include <stdlib.h>
 
-#define IP "142.251.43.99" //Ip of google.com, to get this just do `host www.google.com` or ping google
-#define PORT 80
+#define IP "142.251.43.99" //Ip of google.com, to get this just do `host www.google.com` or ping google.
+#define PORT 80 
 
 int main(){
    int s;
-  struct sockaddr_in sock;
-   char buf[512];
+  struct sockaddr_in sock; //exat name sockaddr_in is compulsory.
+   char buf[512];// 512 size of buffer is perfect for test. Long buffer size will capture long datas from socket. 512 buffer size just holds OK http response and little information about response.
    char *data;
-   data="HEAD / HTTP/1.0\n\n";
+   data="HEAD / HTTP/1.0\n\n"; //Sockets tries to connect using this header and response will be printed on below printf("").
 
    s=socket(AF_INET, SOCK_STREAM,0);
 
    if (s<0){
-      printf("socket() error\n");
+      printf("socket() error\n");//we also can eliminate socket(). It is just to display error.
       return -1;
    }
    
@@ -36,6 +36,6 @@ int main(){
    write (s,data,strlen(data) );
    read(s,buf,511);
    close(s);
-   printf("\n%s\n",buf);
+   printf("%s\n",buf);
    return 0;
 }
